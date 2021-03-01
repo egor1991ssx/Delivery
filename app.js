24fc86e60fc29ae23eea647c78b242907d60d729
Сафронов Егор <e.safronov@kvantorium62.local>
2021-03-01 18:11:15 +0300

    10 часов кодил, сори

--- app.js
+++ app.js
@@ -16,10 +16,22 @@ function renderProducts(container, products) {
             <div>${product.name}</div>
             <div>${product.description}</div>
             <div>${product.price}₽</div>
+            <a class="button-js">Заказать</a>
           </div>`;
     container.appendChild(productElement);
   });
 }
+
+
+
+let news = [
+  new Product("Сет1", "Афигенный сет", "set1.jpg", 1200),
+  new Product("Сет2", "Вкусный сет", "set2.jpg", 1300),
+  new Product("Сет3", "Классный сет", "set3.jpg", 1400),
+];
+let newsElement = document.getElementById("news");
+
+renderProducts(newsElement, news);
 
 let sets = [
   new Product("Сет1", "Афигенный сет", "set1.jpg", 1200),
@@ -31,6 +43,9 @@ let setsElement = document.getElementById("sets");
 renderProducts(setsElement, sets);
 
 let classic = [
+  new Product("Ролл1", "Афигенный сет", "set1.jpg", 400),
+  new Product("Ролл1", "Афигенный сет", "set1.jpg", 400),
+  new Product("Ролл1", "Афигенный сет", "set1.jpg", 400),
   new Product("Ролл1", "Афигенный сет", "set1.jpg", 400),
   new Product("Ролл2", "Вкусный сет", "set2.jpg", 500),
   new Product("Ролл3", "Классный сет", "set3.jpg", 600),
@@ -47,6 +62,34 @@ let hot = [
 let hotElement = document.getElementById("hot");
 
 renderProducts(hotElement, hot);
+
+let baked = [
+  new Product("Сет1", "Афигенный сет", "set1.jpg", 1200),
+  new Product("Сет2", "Вкусный сет", "set2.jpg", 1300),
+  new Product("Сет3", "Классный сет", "set3.jpg", 1400),
+];
+let bakedElement = document.getElementById("baked");
+
+renderProducts(bakedElement, baked);
+
+let sushi = [
+  new Product("Ролл1", "Афигенный сет", "set1.jpg", 400),
+  new Product("Ролл2", "Вкусный сет", "set2.jpg", 500),
+  new Product("Ролл3", "Классный сет", "set3.jpg", 600),
+];
+let sushiElement = document.getElementById("sushi");
+
+renderProducts(sushiElement, sushi);
+
+
+let wok = [
+  new Product("Ролл1", "Афигенный сет", "set1.jpg", 400),
+  new Product("Ролл2", "Вкусный сет", "set2.jpg", 500),
+  new Product("Ролл3", "Классный сет", "set3.jpg", 600),
+];
+let wokElement = document.getElementById("wok");
+
+renderProducts(wokElement, wok);
 
 let salad = [
   new Product("Ролл1", "Афигенный сет", "set1.jpg", 400),
@@ -65,3 +108,36 @@ let drink = [
 let drinkElement = document.getElementById("drink");
 
 renderProducts(drinkElement, drink);
+
+setTimeout(function(){
+	document.body.classList.add('body_visible');
+}, 200);
+
+
+var h_hght = 0; // высота шапки
+var h_mrg = 0;    // отступ когда шапка уже не видна
+                 
+$(function(){
+ 
+    var elem = $('#menu');
+    var top = $(this).scrollTop();
+     
+    if(top > h_hght){
+        elem.css('top', h_mrg);
+    }           
+     
+    $(window).scroll(function(){
+        top = $(this).scrollTop();
+         
+        if (top+h_mrg < h_hght) {
+            elem.css('top', (h_hght-top));
+        } else {
+            elem.css('top', h_mrg);
+        }
+    });
+ 
+});
+
+window.addEventListener('scroll', function() {
+  document.getElementById('showScroll').innerHTML = pageYOffset + '30px';
+});
